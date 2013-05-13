e90ae78fc14fe8b7833b05aa371f79ec38721ff1
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-05-13 15:33:11 +0200

    Some refactoring; support for https://github.com/timmolter/XChange/issues/114

--- src/main/java/si/mazi/rescu/Params.java
+++ src/main/java/si/mazi/rescu/Params.java
@@ -134,7 +134,7 @@ public class Params implements Serializable {
         return paramValue.toString();
     }
 
-    public void digestAll(RestInvocationParams invocationParams) {
+    public void digestAll(RestMethodMetadata restMethodMetadata, RestInvocationParams invocationParams) {
         for (String paramName : data.keySet()) {
             Object paramValue = data.get(paramName);
             if (paramValue instanceof ParamsDigest) {
