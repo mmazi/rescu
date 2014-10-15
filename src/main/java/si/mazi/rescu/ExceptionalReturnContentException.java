2fce55756090a697a441eb633c798872e7562959
mazi <matija.mazi@gmail.com>
2014-10-15 09:51:14 +0200

    #47 added javadocs

--- src/main/java/si/mazi/rescu/ExceptionalReturnContentException.java
+++ src/main/java/si/mazi/rescu/ExceptionalReturnContentException.java
@@ -1,5 +1,9 @@
 package si.mazi.rescu;
 
+/**
+ * Throw this exception (eg. in your json model class constructors) to signal that rescu
+ * should parse the response body as exception. Declare the exception type on the method.
+ */
 public class ExceptionalReturnContentException extends RuntimeException {
     public ExceptionalReturnContentException(String message) {
         super(message);
