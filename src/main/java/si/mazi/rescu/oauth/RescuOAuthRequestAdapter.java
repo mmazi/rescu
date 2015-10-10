7e41729e9b773a512d3e2653e512b94f365f6bdf
Rafał Krupiński <rkrupinski+github@gmail.com>
2015-10-10 19:25:30 +0100

    Make OAuth support application/x-www-form-urlencoded

---
+++ src/main/java/si/mazi/rescu/oauth/RescuOAuthRequestAdapter.java
@@ -0,0 +1,25 @@
+package si.mazi.rescu.oauth;
+
+import oauth.signpost.basic.HttpURLConnectionRequestAdapter;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+/**
+ * @author Rafał Krupiński
+ */
+public class RescuOAuthRequestAdapter extends HttpURLConnectionRequestAdapter {
+    private String messagePayload;
+
+    public RescuOAuthRequestAdapter(HttpURLConnection connection, String messagePayload) {
+        super(connection);
+        this.messagePayload = messagePayload;
+    }
+
+    @Override
+    public InputStream getMessagePayload() throws IOException {
+        return messagePayload != null ? new ByteArrayInputStream(messagePayload.getBytes()) : null;
+    }
+}
