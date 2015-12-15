7d404cd8e3c072d001e59a8615749060ed288a8c
Matija Mazi <matija.mazi@gmail.com>
2015-12-15 10:36:01 +0100

    Revert: Suppress duplicate code IDE warnings in test.

--- src/test/java/si/mazi/rescu/RestInvocationHandlerTest.java
+++ src/test/java/si/mazi/rescu/RestInvocationHandlerTest.java
@@ -55,7 +55,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author Matija Mazi
  */
-@SuppressWarnings("Duplicates")
+//@SuppressWarnings("Duplicates")
 public class RestInvocationHandlerTest {
 
     private static final Logger log = LoggerFactory.getLogger(RestInvocationHandlerTest.class);
