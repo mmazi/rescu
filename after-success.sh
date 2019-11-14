2ca06f62c54e999b50a5ea758fac8071f26fb0f7
Matija Mazi <matija.mazi@gmail.com>
2019-11-14 09:39:47 +0100

    Use openjdk8 instead of oraclejdk8 as the environment for deploying maven snapshots. Add other JDKs.

--- after-success.sh
+++ after-success.sh
@@ -5,7 +5,7 @@ GIT_BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 echo "Travis branch:       " ${TRAVIS_BRANCH}
 echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
 echo "Travis JDK version:  " ${TRAVIS_JDK_VERSION}
-if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" -a "${TRAVIS_BRANCH}" == "develop" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+if [ "${TRAVIS_JDK_VERSION}" == "openjdk8" -a "${TRAVIS_BRANCH}" == "develop" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     mvn deploy --settings settings.xml -P sources-docs
 else
     echo "Not deploying artefacts. This is only done with non-pull-request commits to develop branch with Java 8 builds."
