4883602e9f1e0a82d9e9a09df2533161754548a7
Pavel Niedoba <pavel.niedoba.75@gmail.com>
2020-05-20 12:07:15 +0200

    debug logging of request/response  (#117)
    Modified logging so better fit production by adding DEBUG, which logs responses truncated at some maximum length
    
    Co-authored-by: pavel <bbb>

--- src/main/java/si/mazi/rescu/HttpTemplate.java
+++ src/main/java/si/mazi/rescu/HttpTemplate.java
@@ -33,8 +33,16 @@ import si.mazi.rescu.utils.HttpUtils;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,8 +52,20 @@ import java.util.zip.GZIPInputStream;
  * Various HTTP utility methods
  */
 class HttpTemplate {
-
     public final static String CHARSET_UTF_8 = "UTF-8";
+
+
+    /**
+     * if log level DEBUG is set to this class, response body will be logged.
+     * Maximum logged length is truncated at this static variable
+     */
+    public static int responseMaxLogLen = 4096;
+    
+    /**
+     * if log level DEBUG is set to this class, request body will be logged.
+     * Maximum logged length is truncated at this static variable
+     */
+    public static int requestMaxLogLen = 4096;
 
     private final Logger log = LoggerFactory.getLogger(HttpTemplate.class);
 
@@ -65,7 +85,7 @@ class HttpTemplate {
                  SSLSocketFactory sslSocketFactory, HostnameVerifier hostnameVerifier, OAuthConsumer oAuthConsumer) {
       this(0, readTimeout, proxyHost, proxyPort, sslSocketFactory, hostnameVerifier, oAuthConsumer);
     }
-    
+
     HttpTemplate(int connTimeout, int readTimeout, String proxyHost, Integer proxyPort,
                  SSLSocketFactory sslSocketFactory, HostnameVerifier hostnameVerifier, OAuthConsumer oAuthConsumer) {
         this.connTimeout = connTimeout;
@@ -89,8 +109,11 @@ class HttpTemplate {
     }
 
     HttpURLConnection send(String urlString, String requestBody, Map<String, String> httpHeaders, HttpMethod method) throws IOException {
-        log.debug("Executing {} request at {}", method, urlString);
-        log.trace("Request body = {}", requestBody);
+        if (requestBody != null && requestBody.length() > 0) {
+            log.debug("Executing {} request at {}  body \n{}", method, urlString, truncate(requestBody, requestMaxLogLen));
+        } else {
+            log.debug("Executing {} request at {}", method, urlString);
+        }
         log.trace("Request headers = {}", httpHeaders);
 
         preconditionNotNull(urlString, "urlString cannot be null");
@@ -129,13 +152,12 @@ class HttpTemplate {
             }
         }
 
-        InputStream inputStream = !HttpUtils.isErrorStatusCode(httpStatus) ?
-            connection.getInputStream() : connection.getErrorStream();
+        InputStream inputStream = !HttpUtils.isErrorStatusCode(httpStatus) ? connection.getInputStream() : connection.getErrorStream();
         String responseString = readInputStreamAsEncodedString(inputStream, connection);
         if (responseString != null && responseString.startsWith("\uFEFF")) {
             responseString = responseString.substring(1);
         }
-        log.trace("Http call returned {}; response body:\n{}", httpStatus, responseString);
+        log.debug("Http call returned {}; response body:\n{}", httpStatus, truncate(responseString, responseMaxLogLen));
 
         return new InvocationResult(responseString, httpStatus);
     }
@@ -187,19 +209,19 @@ class HttpTemplate {
         if (connTimeout > 0) {
           connection.setConnectTimeout(connTimeout);
         }
-        
+
         if (connection instanceof HttpsURLConnection) {
-            HttpsURLConnection httpsConnection = (HttpsURLConnection)connection;
-            
+            HttpsURLConnection httpsConnection = (HttpsURLConnection) connection;
+
             if (sslSocketFactory != null) {
                 httpsConnection.setSSLSocketFactory(sslSocketFactory);
             }
-            
+
             if (hostnameVerifier != null) {
                 httpsConnection.setHostnameVerifier(hostnameVerifier);
             }
         }
-        
+
         return connection;
     }
 
@@ -208,8 +230,8 @@ class HttpTemplate {
      * Reads an InputStream as a String allowing for different encoding types. This closes the stream at the end.
      * </p>
      *
-     * @param inputStream      The input stream
-     * @param connection     The HTTP connection object
+     * @param inputStream The input stream
+     * @param connection  The HTTP connection object
      * @return A String representation of the input stream
      * @throws IOException If something goes wrong
      */
@@ -235,7 +257,10 @@ class HttpTemplate {
         } finally {
             inputStream.close();
             if (reader != null) {
-                try { reader.close(); } catch (IOException ignore) { }
+                try {
+                    reader.close();
+                } catch (IOException ignore) {
+                }
             }
         }
     }
@@ -272,4 +297,19 @@ class HttpTemplate {
         }
     }
 
+    /**
+     * This will truncate string toTruncate to maximum length or return it as is if shorter
+     * @param toTruncate
+     * @param maxLen
+     * @return
+     */
+    private String truncate(String toTruncate, int maxLen) {
+        if (toTruncate == null) {
+            return null;
+        }
+        if (toTruncate.length() <= maxLen) {
+            return toTruncate;
+        }
+        return toTruncate.substring(0, maxLen);
+    }
 }
